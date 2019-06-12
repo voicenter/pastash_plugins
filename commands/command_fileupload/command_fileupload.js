ce7750f1e520ee4d3edfb117c2a035cc6d4393d1
Maksym Kyrylov <kirillovmax@gmail.com>
2019-06-12 15:02:43 +0300

    Added download functionality to command_fileupload command

--- commands/command_fileupload/command_fileupload.js
+++ commands/command_fileupload/command_fileupload.js
@@ -4,41 +4,96 @@
  */
 
 const PromiseFtp = require("promise-ftp");
+const fs = require('fs');
 
 let conf;
 const defaultConf = {
-  pluginFieldName: 'FieTransfer',
+  pluginFieldName: 'FileTransfer',
   port: 21,
-  usarname: 'anonymous',
+  username: 'anonymous',
+  secure: false
 };
 
 module.exports = function plugin(userConf) {
-  conf = { ...defaultConf, ...userConf };
+  conf = {...defaultConf, ...userConf};
 
   this.main.uploadFile = function uploadFile(next) {
     const data = this.data[conf.pluginFieldName];
-
-    var ftp = new PromiseFtp();
+    const ftp = new PromiseFtp();
     ftp.connect({
       host: conf.host,
       port: conf.port,
-      user: conf.usarname,
-      password: conf.password
+      user: conf.username,
+      password: conf.password,
+      secure: conf.secure
     })
       .then(() => {
-        ftp.put(
-          data[conf.inputFileField] + data[conf.nameField],
-          data[conf.outputFileField] + data[conf.nameField]);
-      }).then(() => {
-        ftp.list(data[conf.outputFileField]).then((list) => {
+        return ftp.put(
+          data[conf.uploadInputFileField] + data[conf.nameField],
+          data[conf.uploadOutputFileField] + data[conf.nameField]);
+      })
+      .then(() => {
+        return ftp.list(data[conf.uploadOutputFileField]).then((list) => {
           const size = list.find(it => it.name === data[conf.nameField]).size;
-          if (size !== data[conf.sizeField]) {
-            console.log('file size not to match');
+          if (parseInt(size) !== parseInt(data[conf.sizeField])) {
+            console.log('File size does not match on upload');
           } else {
             ftp.end();
-            next();
+            return next();
           }
-        });
+        })
+      })
+      .catch((err) => {
+        console.log(err);
+        ftp.end();
+        return next();
       });
-  }
-}
+  };
+
+  this.main.downloadFile = function downloadFile(next) {
+    console.log(this.data);
+
+    const data = this.data[conf.pluginFieldName];
+    const ftp = new PromiseFtp();
+    ftp.connect({
+      host: conf.host,
+      port: conf.port,
+      user: conf.username,
+      password: conf.password,
+      secure: conf.secure
+    })
+      .then(() => {
+        return ftp.get(data[conf.downloadInputFileField] + data[conf.nameField]);
+      })
+      .then((stream) => {
+        return new Promise(function (resolve, reject) {
+          stream.once('close', resolve);
+          stream.once('error', reject);
+          stream.pipe(fs.createWriteStream(data[conf.downloadOutputFileField] + data[conf.nameField] + '.copy'));
+        });
+      })
+      .then(() => {
+        return new Promise(function (resolve) {
+          fs.stat(data[conf.downloadOutputFileField] + data[conf.nameField] + '.copy', (err, stats) => {
+            if (err) {
+              throw err;
+            }
+            resolve(stats);
+          });
+        });
+      })
+      .then((stats) => {
+        if (parseInt(stats.size) !== parseInt(data[conf.sizeField])) {
+          console.log('File size does not match on download');
+        }
+        ftp.end();
+        return next();
+      })
+      .catch((err) => {
+        console.log(err);
+        console.log('Failed on file download');
+        ftp.end();
+        return next();
+      });
+  };
+};
--- commands/command_fileupload/command_fileupload.md
+++ commands/command_fileupload/command_fileupload.md
@@ -1,22 +1,46 @@
-Upload and check file command plugin
+Upload/Download and check file command plugin
 ---
 
 Config example:
 ````
 "@voicenter/voicenter_pastash_command_fileupload": {
-    "pluginFieldName": "FieTransfer",
-    "inputFileField": "sourceFilePath",
-    "outputFileField": "destFilePath",
-    "nameField": "fileName",
-    "sizeField": "fileSize",
+    "pluginFieldName": "FileTransfer",  // Host parameter name for particular plugin
+    "uploadInputFileField": "uploadSourceFilePath", // Input file path for upload. 
+    "uploadOutputFileField": "uploadDestFilePath",  // Output file path for upload.
+    "downloadInputFileField": "downloadSourceFilePath", // Input file path for download. 
+    "downloadOutputFileField": "downloadDestFilePath",  // Output file path for download.
+    "nameField": "fileName", // File name
+    "sizeField": "fileSize", // File size for checking
+    
+    // FTP params
     "host": "host",
     "usarname": "usarname",
     "password": "password",
-    "port": "port"
+    "port": "port",
+    "secure": true
+}
+````
+
+Example of JSON payload for such configuration in case of 'field' parameter of Pastash configuration value is 'Command' :
+````
+{
+  "@timestamp": "4200000000",
+  "Command": {
+    "FileTransfer": {
+      "uploadSourceFilePath": "/local/path",
+      "uploadDestFilePath": "/remote/path",
+      "downloadSourceFilePath": "/remote/path",
+      "downloadDestFilePath": "/local/path",
+
+      "fileName": "file.name",
+      "fileSize": "42"
+    }
+  }
 }
 ````
 
 Commands list:
 ````
 uploadFile();
+downloadFile();
 ````
