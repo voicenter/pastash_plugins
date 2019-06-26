5e3b998cca0a36955b6f93e6002a9bc16e653b3b
Maksym Kyrylov <kirillovmax@gmail.com>
2019-06-26 17:15:44 +0300

    crypto pmx monitoring

--- commands/command_cryptofile/command_cryptofile.js
+++ commands/command_cryptofile/command_cryptofile.js
@@ -4,6 +4,7 @@
  */
 
 const encryptor = require('file-encryptor');
+const pmx = require('pmx');
 let conf;
 const defaultConf = {
   algorithm: 'aes256',
@@ -12,17 +13,25 @@ const defaultConf = {
 
 module.exports = function plugin(userConf) {
   conf = { ...defaultConf, ...userConf };
+
+  let probe = pmx.probe();
+  let f_counter = probe.counter({
+    name: 'Encrypting/Decrypting files'
+  });
+
   const options = {
     algorithm: conf.algorithm,
   };
 
   this.main.encryptFile = function encryptFile(next) {
     const data = this.data[conf.pluginFieldName];
+    f_counter.inc();
     encryptor.encryptFile(data[conf.inputFileField], data[conf.outputFileField], data[conf.keyField], options, (err) => {
       this.data.Result.push({
         plugin: conf.pluginFieldName,
         response: err ? 500 : 200
       });
+      f_counter.dec();
       next();
     });
   };
