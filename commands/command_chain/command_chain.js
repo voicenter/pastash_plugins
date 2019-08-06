b4e5e25c52da99f5cf09f573944e1f900b728b28
Shlomi  Gutman <shlomi@voicenter.com>
2019-08-06 03:01:14 +0200

    Add Tigran Mail PlugIns

--- commands/command_chain/command_chain.js
+++ commands/command_chain/command_chain.js
@@ -11,12 +11,12 @@ module.exports = function plugin(userConf) {
     this.main.chain = function (next) {
         // calling next  this to allow chaining on this function
         next();
-    }
+    };
     //Some demo staff nedd to clean there for async test concept 
-    this.main.fooFunc = demoFuncaion
-    this.main.barFunc = demoFuncaion
+    this.main.fooFunc = demoFuncaion;
+    this.main.barFunc = demoFuncaion;
     this.main.bazFunc = demoFuncaion
-}
+};
 //Some demo staff nedd to clean there for async test concept 
 var demoFuncaion = function (next) {
     console.log(CircularJSON.stringify(this.data));
@@ -24,6 +24,7 @@ var demoFuncaion = function (next) {
     setTimeout(function () {
         next(null, arguments.callee.toString().substr(0, 3) + ' value'); // Continue on with 'foo value', 'bar value' etc.
         console.log(arguments.callee.name)
+        console.log("Shlomi !!!!!!!!!!!!!")
     }.bind(this), Math.random() * conf.timeout); // After a random wait of up to a second
 
 };
